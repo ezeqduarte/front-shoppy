7642bbaf33dc18395c04ce752cfdf44e3aec681c
Your Name <ezeqduarte01@gmail.com>
2022-12-12 20:10:07 -0300

    video en bienvenida

--- src/pages/Bienvenida/Bienvenida.jsx
+++ src/pages/Bienvenida/Bienvenida.jsx
@@ -4,18 +4,28 @@ import "../Bienvenida/bienvenida.css";
 
 export default function Bienvenida() {
   return (
-    <div className="mainBienvenida">
-      <div className="divBienvenida">
-        <h2>
-          BIENVENIDOS A SHOPPY<span className="blanco">.</span>
-        </h2>
-        <p className="parrafoBienvenida">
-          Te invitamos a conocer nuestra tienda y todos nuestros productos
-        </p>
-        <NavLink to={`/inicio`} style={{ textDecoration: "none" }}>
-          <p className="buttonBienvenida">INGRESA</p>
-        </NavLink>
+    <>
+      <div className="mainBienvenida">
+        <video muted autoPlay loop>
+          <source
+            src="https://cdn.discordapp.com/attachments/830354293822324736/1051997742244835360/pexels-rodnae-productions-7914777.mp4"
+            type="video/mp4"
+          />
+        </video>
+        <div className="capa"></div>
+
+        <div className="divBienvenida">
+          <h2>
+            BIENVENIDOS A SHOPPY<span className="blanco">.</span>
+          </h2>
+          <p className="parrafoBienvenida">
+            Te invitamos a conocer nuestra tienda y todos nuestros productos
+          </p>
+          <NavLink to={`/inicio`} style={{ textDecoration: "none" }}>
+            <p className="buttonBienvenida">INGRESA</p>
+          </NavLink>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
