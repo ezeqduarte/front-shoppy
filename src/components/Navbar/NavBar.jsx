424bc7e64ec74ad184048fb2f890cbb65e8a2570
Your Name <ezeqduarte01@gmail.com>
2022-12-12 03:37:23 -0300

    navbar lista

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,5 +1,23 @@
 import React from "react";
+import "../Navbar/navbar.css";
 
 export default function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <div className="navbar">
+      <img
+        src="https://cdn.discordapp.com/attachments/830354293822324736/1051744433550397510/Sin_titulo-2.png"
+        alt="logo_shoppy"
+      />
+      <ul>
+        <li>PRODUCTOS</li>
+        <li>INICIO</li>
+        <li>CONTACTO</li>
+      </ul>
+      <div className="iconos">
+        <span class="material-symbols-outlined">account_circle</span>
+        <span class="material-symbols-outlined">favorite</span>
+        <span class="material-symbols-outlined">shopping_cart</span>
+      </div>
+    </div>
+  );
 }
