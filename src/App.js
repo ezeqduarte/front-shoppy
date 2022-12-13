4e89fc038f57ac29648604a886e582ff101570b0
MatiasCalvi <alejandropreuss30@gmail.com>
2022-12-13 07:22:28 -0300

    productos

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Main from "./layouts/Main";
 import Bienvenida from "./pages/Bienvenida/Bienvenida";
 import Inicio from "./pages/Inicio/Inicio";
+import Productos from "./pages/Productos/Productos"
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Main>
         <Routes>
           <Route path="/inicio" element={<Inicio />} />
+          <Route path="/productos" element={<Productos/>} />
         </Routes>
       </Main>
     </>
