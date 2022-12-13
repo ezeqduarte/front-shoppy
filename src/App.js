d590a16a5a424eb0a6ec5170127594c3b37fdb56
Your Name <ezeqduarte01@gmail.com>
2022-12-13 20:54:40 -0300

    pagina signIn lista de diseño

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import Main from "./layouts/Main";
 import Bienvenida from "./pages/Bienvenida/Bienvenida";
+import Ingresar from "./pages/Ingresar/Ingresar";
 import Inicio from "./pages/Inicio/Inicio";
-import Productos from "./pages/Productos/Productos"
+import Productos from "./pages/Productos/Productos";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="" element={<Bienvenida />} />
+        <Route path="/ingresar" element={<Ingresar />} />
       </Routes>
       <Main>
         <Routes>
           <Route path="/inicio" element={<Inicio />} />
-          <Route path="/productos" element={<Productos/>} />
+          <Route path="/productos" element={<Productos />} />
         </Routes>
       </Main>
     </>
---
+++ src/pages/Ingresar/Ingresar.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "./ingresar.css";
+
+export default function Ingresar() {
+  return (
+    <div className="Ingreso">
+      <img
+        className="shoppyLogoIngreso"
+        src="https://cdn.discordapp.com/attachments/830354293822324736/1051744433550397510/Sin_titulo-2.png"
+        alt="logo_shoppy"
+      />
+      <div className="edFotoIngreso"></div>
+      <div className="edFormIngreso">
+        <h2>
+          INGRESO<span className="blanco">.</span>
+        </h2>
+        <div>
+          <form>
+            <label>
+              EMAIL
+              <input type="email" />
+            </label>
+            <label>
+              PASSWORD
+              <input type="password" />
+            </label>
+          </form>
+          <div className="botonesForm">
+            <p>REGISTRARME</p>
+            <button>INGRESA A SHOPPY</button>
+          </div>
+        </div>
+      </div>
+      <div className="edVolverAInicio">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="26"
+          height="26"
+          color="#f3f3f3"
+          fill="currentColor"
+          class="bi bi-house"
+          viewBox="0 0 16 16"
+        >
+          <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z" />
+        </svg>
+        <p>volver</p>
+      </div>
+    </div>
+  );
+}
